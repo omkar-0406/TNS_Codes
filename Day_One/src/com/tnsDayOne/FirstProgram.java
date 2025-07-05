65e2a14f8c2b7868b39d29d69b33c6c9e1bb67bb
omkar <omkar@Omkar>
2025-07-05 23:22:47 +0530

    Day1 Data Types

---
+++ Day_One/src/com/tnsDayOne/FirstProgram.java
@@ -0,0 +1,17 @@
+package com.tnsDayOne;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		int a = 2333;
+		System.out.println("Value of a: "+ a);
+		
+		int x = 45;
+		int y = 60;
+		System.out.println("Addition is: "+ (x+y));
+	}
+
+}
---
+++ Day_One/src/com/tnsDayOne/PrimitiveDataTypes.java
@@ -0,0 +1,30 @@
+package com.tnsDayOne;
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		
+		int a = 232222;
+		System.out.println(a);
+		
+		byte b = 10;
+		System.out.println(b);
+		
+		long l = 105555523344555555L;
+		System.out.println(l);
+		
+		short s = 4567;
+		System.out.println(s);
+		
+		char c = 'D';
+		System.out.println(c);
+		
+		float f = 10.2222f;
+		System.out.println(f);
+		
+		double d = 1234.5555555d;
+		System.out.println(d);
+		
+		boolean bool = true;
+		System.out.println(bool);
+	}
+}
